335ec8ac9c457f7cb9925cf863cfd415c5929b81
FaresYch <114227286+FaresYch@users.noreply.github.com>
2025-01-23 13:25:57 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -77,7 +77,7 @@ def enregistrer_client():
     conn.close()
     return redirect('/consultation/')  # Rediriger vers la page d'accueil après l'enregistrement
 
-@app.route('./fiche_nomtest', methods=['GET'])
+@app.route('/fiche_nom', methods=['GET'])
 def fiche_nom_get():
     return render_template('formulaire_nom.html')
     
