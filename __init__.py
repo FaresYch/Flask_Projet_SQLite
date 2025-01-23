aba1c8d54fe5b08880310d3ce21cbfb39f8170fc
FaresYch <114227286+FaresYch@users.noreply.github.com>
2025-01-23 10:20:04 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -93,6 +93,13 @@ def fiche_nom_post():
     # Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
 
+@app.route('/resultat_recherche/')
+def ReadBDD():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM clients;')
+    conn.close()
+    return render_template('read_data.html', data=data)
                                                                                                              
 if __name__ == "__main__":
   app.run(debug=True)
