7ae22fb75487394f886e0b07345a73872a6d1122
FaresYch <114227286+FaresYch@users.noreply.github.com>
2025-01-23 10:52:37 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -87,17 +87,17 @@ def fiche_nom_post():
 
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM clients WHERE nom = ?', (nom_client))
+    cursor.execute('SELECT * FROM clients WHERE nom = ?;', (nom_client))
     data = cursor.fetchall()
     conn.close()
     # Rendre le template HTML et transmettre les données
-    return render_template('/consultation/', data=data)
+    return render_template('read_data.html', data=data)
 
 @app.route('/resultat_recherche/')
 def ReadBDDRecherche():
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM clients;')
+    cursor.execute('SELECT * FROM clients WHERE nom = ?')
     conn.close()
     return render_template('read_data.html', data=data)
                                                                                                              
